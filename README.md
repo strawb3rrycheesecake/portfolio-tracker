ac014bef9101f1d87cd990f7124e16b300c5da1b
strawb3rrycheesecake <calvinngkm@gmail.com>
2021-12-02 20:39:13 +0800

    021221

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 I wanted to write a script to help me track my time weighted returns of a portfolio by reading it off a excel or csv file.
 For simplicity's sake, I will first leave out dividends.
 I will be getting my financial data from yahoo finance and the frequency of the recording will be weekly
+
+271121
+The End date refers to when was the last time we held the position. If we are still holding the position, the End date will be today. If we sold, it will be the date that we sold it.
--- portfolio_tracker.py
+++ portfolio_tracker.py
@@ -5,18 +5,46 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from pandas.core.indexes.base import Index
 import pandas_datareader.data as web
 import datetime as dt
+#setting today's date
+tdy = dt.date.today().strftime('%Y-%m-%d')
+print(tdy)
 
-#importing portfolio from excel or csv file
-pf = pd.read_excel("test portfolio.xlsx")
+#importing transactions from excel or csv file
+pf = pd.read_excel("test_portfolio.xlsx")
 
 #changing date, bought date, and sold date to datetime format
-pf['Date'] = pd.to_datetime(pf['Date'])
 pf['Bought date'] = pd.to_datetime(pf['Bought date'])
 pf['Sold date'] = pd.to_datetime(pf['Sold date'])
+pf['End date'] = pd.to_datetime(pf['End date'])
+#calculating cost price for each transaction
+#I did this in python instead of in the excel sheet as it will mess up the formating when i try to read the file
+pf['Cost price'] = pf['Units'] * pf['Bought price']
 
+#i want to be able to iterate through the transaction list and print a plot showing the time-weighted return of a particular stock
+#determining end date
+def end_date(param):#the param in this case has to be a dataframe consisting of the transactions
+    for i in range(len(param.index)):
+        if pd.isnull(param['Sold date'][i]) is True:
+            param['End date'][i] = tdy
+        else:
+            param['End date'][i] = param['Sold date'][i]
+
+end_date(pf)
 print(pf)
-df = web.DataReader('ISRG', 'yahoo', pf['Bought date'][1], pf['Date'][1])
-df.index() = pd.to_datetime(df.index())
-print(type(df.index()))
+
+def hold_period(param):
+    title = ['Date']
+    tick = pf['Ticker'].values.tolist()
+    for i in tick:
+        title.append(i)
+
+hold_period(pf)
+
+print(pf['Ticker'][0])
+
+swrd = web.DataReader(pf['Ticker'][0], 'yahoo', pf['Bought date'][0], pf['End date'][0])['Adj Close']
+swrd = swrd.values.tolist()
+print(swrd)
